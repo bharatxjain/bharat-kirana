76ddabbc446a45b108a439dc79c47a070191509d
Bharat Jain <bharatjain@Bharats-MacBook-Air.local>
2026-08-26 22:24:48 +0530

    Bug Fixed.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
     applicationId = "com.kks.bharatkirana"
     minSdk = 24
     targetSdk = 35
-    versionCode = 7
-    versionName = "1.07"
+    versionCode = 8
+    versionName = "1.08"
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
   }
